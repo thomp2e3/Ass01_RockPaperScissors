0662cdc6f538de53426c66ccf0707bf0a661c44d
thomp2e3 <thomp2e3@mail.uc.edu>
2024-10-03 01:04:46 -0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -8,54 +8,81 @@ public class Main
 
         String play1 = "";
         String play2 = "";
+        String confirmYN = "";
 
-        System.out.println("Player 1, enter your move [RPS]: ");
-        play1 = in.nextLine();
-        System.out.println("Player 2, enter your move [RPS]: ");
-        play2 = in.nextLine();
+        boolean done = false;
 
-        if (play1.equalsIgnoreCase("R"))
-            if (play2.equalsIgnoreCase("R"))
-            {
-                System.out.println("Rock vs Rock... Game is a tie!");
-            }
-            else if (play2.equalsIgnoreCase("P"))
-            {
-                System.out.println("Paper beats Rock... Player 2 wins!");
-            }
-            else if (play2.equalsIgnoreCase("S"))
-            {
-                System.out.println("Rock beats Scissors... Player 1 wins");
-            }
+        do {
+            System.out.println("Player 1, enter your move [RPS]: ");
+            play1 = in.nextLine();
+            System.out.println("Player 2, enter your move [RPS]: ");
+            play2 = in.nextLine();
 
-        if (play1.equalsIgnoreCase("P"))
-            if (play2.equalsIgnoreCase("R"))
-            {
-                System.out.println("Paper beats Rock... Player 1 wins!");
-            }
-            else if (play2.equalsIgnoreCase("P"))
-            {
-                System.out.println("Paper vs Paper... Game is a tie!");
-            }
-            else if (play2.equalsIgnoreCase("S"))
-            {
-                System.out.println("Scissors beats Paper... Player 2 wins");
-            }
 
-        if (play1.equalsIgnoreCase("S"))
-            if (play2.equalsIgnoreCase("R"))
-            {
-                System.out.println("Rock beats Scissors... Player 2 wins!");
-            }
-           else if (play2.equalsIgnoreCase("P"))
-            {
-                System.out.println("Scissors beats Paper... Player 1 wins!");
-            }
-            else if (play2.equalsIgnoreCase("S"))
-            {
-                System.out.println("Scissors vs Scissors... Game is a tie!");
-            }
-        else
-                System.out.println("Invalid move! Run the program again!");
-        }
+            if (play1.equalsIgnoreCase("R"))
+                if (play2.equalsIgnoreCase("R"))
+                {
+                    System.out.println("Rock vs Rock... Game is a tie!");
+                }
+                else if (play2.equalsIgnoreCase("P"))
+                {
+                    System.out.println("Paper beats Rock... Player 2 wins!");
+                }
+                else if (play2.equalsIgnoreCase("S"))
+                {
+                    System.out.println("Rock beats Scissors... Player 1 wins");
+                }
+
+            if (play1.equalsIgnoreCase("P"))
+                if (play2.equalsIgnoreCase("R"))
+                {
+                    System.out.println("Paper beats Rock... Player 1 wins!");
+                }
+                 else if (play2.equalsIgnoreCase("P"))
+                {
+                    System.out.println("Paper vs Paper... Game is a tie!");
+                }
+                 else if (play2.equalsIgnoreCase("S"))
+                {
+                    System.out.println("Scissors beats Paper... Player 2 wins");
+                }
+
+            if (play1.equalsIgnoreCase("S"))
+                if (play2.equalsIgnoreCase("R"))
+                {
+                    System.out.println("Rock beats Scissors... Player 2 wins!");
+                }
+                 else if (play2.equalsIgnoreCase("P"))
+                 {
+                    System.out.println("Scissors beats Paper... Player 1 wins!");
+                 }
+                 else if (play2.equalsIgnoreCase("S"))
+                {
+                    System.out.println("Scissors vs Scissors... Game is a tie!");
+                }
+            else
+                    System.out.println("Invalid move! Run the program again!");
+
+
+            do {
+                System.out.println("Do you want to continue the game? [Y/N]: ");
+                confirmYN = in.nextLine();
+                if (confirmYN.equalsIgnoreCase("Y"))
+                {
+                    done = false;
+                }
+                else if (confirmYN.equalsIgnoreCase("N"))
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("Invalid input! Please enter Y or N.");
+                }
+            } while (!confirmYN.equalsIgnoreCase("Y") && !confirmYN.equalsIgnoreCase("N"));
+
+
+        } while (!done);
+
+    }
 }
